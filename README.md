6b5ce5a7154f96ba00251d31c128e70978c5726e
Kell <kellspam@outlook.fr>
2019-01-30 18:14:24 +0100

    Teleportation, full restart, custom checkpoints, and multiplayer

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # SADX Quick Restart
+### Description
+This mod allows you, when pressing L + R, to teleport instantly to the previous checkpoint.
+It also lets you save a position by pressing L + LB and then L + R to teleport. Press L + LB again to clear the position. Instead of teleporting, you can do a complete restart (will reload everything) by pressing L + RB.
 
-Immediatly return at last checkpoint on restarting/dying
+### Controls
+| Buttons  | Description                                                             |
+| ---------| ----------------------------------------------------------------------- |
+| `L + R`  | Teleports the current character to last checkpoint / saved position     |
+| `L + RB` | Complete restart without loading screen (player 1 only)                 |
+| `L + LB` | Save/clear the current character saved position                         |
+
+### Installation
+Download MainMemory's [SADX Mod Loader](https://gamebanana.com/tools/6329), hit 'Install URL Handler' in the Options and then download the mod from [here](https://gamebanana.com/gamefiles/8274).
--- sadx-quick-restart/mod.cpp
+++ sadx-quick-restart/mod.cpp
@@ -1,26 +1,98 @@
 #include "stdafx.h"
 
-int temp = 0;
+FunctionPointer(void, SetPlayerInvulnerable, (uint8_t player), 0x441FD0);
+FunctionPointer(void, DeleteSETObjects, (), 0x413F70);
+FunctionPointer(signed int, ResetKnucklesStuff, (), 0x475840);
+ObjectFunc(LoadSETObjects, 0x46C3D0);
+
+DataArray(ObjectFuncPtr, LevelObjects, 0x90BF38, 44);
+RestartData SavedRestartData[8];
+
+void ResetLevel() {
+	DeleteSETObjects();
+	CutsceneMode = 0;
+	DeleteAllObjects();
+	ResetGravity();
+	DisableTimeThing();
+	if (CurrentCharacter == Characters_Knuckles) ResetKnucklesStuff();
+	else if (CurrentCharacter == 7) ResetBigStuff();
+	InitHeap();
+	InitMasterObjectArray();
+	LoadObject(LoadObj_Data1, 8, LoadSETObjects);
+	LoadSkyboxObject();
+	LoadLevelObject();
+	Set0Rings();
+	LoadLevel();
+	GameState = GameMode != GameModes_Trial ? 4 : 23;
+}
+
+void ResetSavedRestartData(uint8_t id) {
+	SavedRestartData[id].Frame = 0;
+}
+
+void ResetTimeAndRings(uint8_t id) {
+	if (!SavedRestartData[id].Frame) SetTime2(RestartLevel.Minute, RestartLevel.Second, RestartLevel.Frame);
+	else SetTime2(SavedRestartData[id].Minute, SavedRestartData[id].Second, SavedRestartData[id].Frame);
+	AddRings(-GetRings());
+	SetPlayerInvulnerable(id);
+	ResetScoreAndBonuses();
+}
+
+void TeleportCharacter(uint8_t id) {
+	EntityData1 * entity = EntityData1Ptrs[id];
+	if (!entity) return;
+
+	if (!SavedRestartData[id].Frame)
+		RestartCheckpoint(&entity->Position, &entity->Rotation);
+	else {
+		entity->Position = SavedRestartData[id].Position;
+		entity->Rotation = SavedRestartData[id].Rotation;
+		ResetTimeAndRings(id);
+	}
+}
+
+void QuickRestart_Handle(uint8_t id) {
+	EntityData1 * entity = EntityData1Ptrs[id];
+
+	//Simple teleport to checkpoint/savedpos
+	if (ControllerPointers[id]->PressedButtons & Buttons_Z) {
+		TeleportCharacter(id);
+	}
+
+	//Reset level, objects and then teleport every player
+	if (ControllerPointers[id]->PressedButtons & Buttons_R && id == 0) {
+		ResetLevel();
+		for (uint8_t i = 0; i < 8; ++i) { TeleportCharacter(id); }
+	}
+
+	//Save a position
+	if (ControllerPointers[id]->PressedButtons & Buttons_L) {
+		if (!SavedRestartData[id].Frame) {
+			SavedRestartData[id].Second = TimeSeconds;
+			SavedRestartData[id].Minute = TimeMinutes;
+			SavedRestartData[id].Frame = TimeFrames;
+			SavedRestartData[id].Position = entity->Position;
+			SavedRestartData[id].Rotation = entity->Rotation;
+		}
+		else {
+			SavedRestartData[id].Frame = 0;
+		}
+	}
+}
 
 extern "C"
 {
 	__declspec(dllexport) void __cdecl OnFrame()
 	{
-		auto entity = EntityData1Ptrs[0];
-		if (entity != nullptr) {
-			if (ControllerPointers[0]->PressedButtons & Buttons_C) {
-				RestartCheckpoint(&entity->Position, &entity->Rotation);
-				entity->Action = 1;
-				temp = 10;
+		if (GameState == 15 || GameState == 9) {
+			for (uint8_t i = 0; i < 8; ++i) {
+				if (ControllerPointers[i]->HeldButtons & Buttons_C) {
+					QuickRestart_Handle(i);
+				}
+
+				if (GameState == 9)
+					ResetSavedRestartData(i);
 			}
-		}
-
-		if (entity == nullptr) return;
-
-		if (temp > 0) {
-			temp -= 1;
-			Camera_Data1->Position = entity->Position;
-			Camera_Data1->Rotation = entity->Rotation;
 		}
 	}
 	
